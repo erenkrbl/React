6b3d34bfd8c137cdedeb96114b2e1609eb131d72
Joseph Abraham <ekurubal24@gmail.com>
2021-06-20 23:51:06 +0300

    Box component added

--- first-project/src/App.js
+++ first-project/src/App.js
@@ -1,5 +1,7 @@
 import logo from './logo.svg';
 import "./App.css";
+import {Card} from './components/Card';
+import Box from  './components/Box';
 
 export const addTwoNumbers1 = (fnumber, snumber) => {
   return fnumber + snumber;
@@ -15,16 +17,8 @@ export function addTwoNumbers3(fnumber, snumber) {
 export default function App() {
   return (
     <div>
-       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-         <p>
-           Clarusway
-        </p>
-        <h1>{addTwoNumbers1(1, 3)}</h1>
-        <h1>{addTwoNumbers2(1, 4)}</h1>
-        <h1>{addTwoNumbers3(1, 5)}</h1>
-        <h1>{getSquare(5)}</h1>         
-       </header>
+       <Card/>
+       <Box/>
     </div>
 
   );
---
+++ first-project/src/components/Box.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Box extends React.Component {
+    render () {
+        return ( 
+            <div className="box-wrapper">
+                BOX
+            </div>
+        )
+    }
+}
+
+export default Box;
