67562eff85a82aeeaa59ecdd4c0bd15d96162528
Joseph Abraham <ekurubal24@gmail.com>
2021-06-21 14:34:33 +0300

    Add card component

--- imdb/src/App.js
+++ imdb/src/App.js
@@ -1,9 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import { Card } from "./components/Card";
 
 function App() {
   return (
     <div className="App">
+      <Card />
       
     </div>
   );
