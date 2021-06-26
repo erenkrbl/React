8c68c54baff2fae92883c326cc93b070368c5c68
Joseph Abraham <ekurubal24@gmail.com>
2021-06-26 12:32:54 +0300

    Added other comments

--- blog-posts/src/index.js
+++ blog-posts/src/index.js
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactDOM from 'react-dom';
-import profile1 from './image/sarah.jpg'
+import profile1 from './image/sarah.jpg';
+import profile2 from './image/antony.jpg';
+import profile3 from './image/helen.jpg';
 
 const App = () => {
     return (
@@ -12,6 +14,42 @@ const App = () => {
                 <div className='content'>
                     <a href="/" className='autor'>
                         Sarah
+                    </a>
+                    <div className='metadata'>
+                        <span className='data'>
+                            Today at 5:00PM
+                        </span>
+                    </div>
+                    <div className='text'>
+                        It's amazing
+                    </div>
+                </div>
+            </div>
+            <div className='comment'>
+                <a href="/" className='avatar'>
+                    <img src={ profile2 } alt="profile picture" /> 
+                </a>
+                <div className='content'>
+                    <a href="/" className='autor'>
+                        Antony
+                    </a>
+                    <div className='metadata'>
+                        <span className='data'>
+                            Today at 5:00PM
+                        </span>
+                    </div>
+                    <div className='text'>
+                        It's amazing
+                    </div>
+                </div>
+            </div>
+            <div className='comment'>
+                <a href="/" className='avatar'>
+                    <img src={ profile3 } alt="profile picture" /> 
+                </a>
+                <div className='content'>
+                    <a href="/" className='autor'>
+                        Helen
                     </a>
                     <div className='metadata'>
                         <span className='data'>
