a8bec288452e473c61956e4d301a7e36d0214b7a
Joseph Abraham <ekurubal24@gmail.com>
2021-06-26 11:34:06 +0300

    Started blog-posts

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,9 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+// function getButtonText(){
+//     return (
+//         "Hi There"
+//     )
+// }
+
 const App =() => {
+    const buttonText = {text: "Submit"};
     return (
-        <div>Hi There!</div>
+        <div>
+            <label htmlFor="name" className="blabel">enter e-mail</label>
+            <input id="name" type="text"/>
+            <button style={{backgroundColor: 'red', color: 'white'}} >{buttonText.text}</button>
+        </div>
     )
 }
 
