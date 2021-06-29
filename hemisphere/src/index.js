22525d8d4d71f87e5f863b223dbcdcba15819945
Joseph Abraham <ekurubal24@gmail.com>
2021-06-29 22:59:59 +0300

    Added geolocation

--- hemisphere/src/index.js
+++ hemisphere/src/index.js
@@ -1,10 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+import HemisphereDisplay from "./hemisphereDisplay";
+window.navigator.geolocation.getCurrentPosition(
+    (position) => console.log(position),
+    (error) => console.log(error)
+);
+
 const App = () => {
     return (
         <div>
             Hello
+            <HemisphereDisplay />
         </div>
     )
 }
