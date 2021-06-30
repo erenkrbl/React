9e6f2c44c6b2a1979dd3c2abcbd3504138f489f5
Joseph Abraham <ekurubal24@gmail.com>
2021-06-30 18:16:05 +0300

    Added ternary operators, image

--- hemisphere/src/hemisphereDisplay.js
+++ hemisphere/src/hemisphereDisplay.js
@@ -1,10 +1,17 @@
 import React from "react";
+import northernPic from "./image/north.jpg";
+import southernPic from "./image/south.jpg";
 
 const HemisphereDisplay = ({ latitude }) => {
-    console.log(latitude)
+
+    //console.log(latitude)
+    const hemisphere = latitude > 0 ? 'Northern hemisphere' : 'Southern hemisphere';
+    const picture = latitude > 0 ? northernPic : southernPic;
+
     return (
         <div>
-            { latitude }
+            { hemisphere }
+            <img src={ picture } alt="hemisphere " />
         </div>
     )
 }
