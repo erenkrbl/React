00b6e31b432b6f2318adc1be34a4a5eb9fb998ca
Joseph Abraham <ekurubal24@gmail.com>
2021-06-30 23:24:15 +0300

    Css componet created

--- hemisphere/src/hemisphereDisplay.js
+++ hemisphere/src/hemisphereDisplay.js
@@ -1,17 +1,35 @@
 import React from "react";
+import './Hemisphere.css';
 import northernPic from "./image/north.jpg";
 import southernPic from "./image/south.jpg";
+
+const hemisphereConfig = {
+    Northern: {
+        text: 'It is northern hemisphere',
+        picture: northernPic
+    },
+    Southern: {
+        text: 'It is southern hemisphere',
+        picture: southernPic
+    }
+}
 
 const HemisphereDisplay = ({ latitude }) => {
 
     //console.log(latitude)
-    const hemisphere = latitude > 0 ? 'Northern hemisphere' : 'Southern hemisphere';
-    const picture = latitude > 0 ? northernPic : southernPic;
+    const hemisphere = latitude > 0 ? 'Northern' : 'Southern';
+    const { text, picture }= hemisphereConfig[hemisphere]
 
     return (
-        <div>
-            { hemisphere }
-            <img src={ picture } alt="hemisphere " />
+        <div className={hemisphere}>
+            <div className='ui raised text container segment'>
+                <div className='image'>
+                    <img src={ picture } alt="hemisphere picture" />
+                </div>
+                <div className='ui teal bottom attached label'>
+                    { text }
+                </div>
+            </div>
         </div>
     )
 }
