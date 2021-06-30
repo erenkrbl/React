b024cfe14d36116bdb7c40a073477a7da28e6e93
Joseph Abraham <ekurubal24@gmail.com>
2021-06-30 13:11:15 +0300

    Converting funct. component to class component

--- hemisphere/src/index.js
+++ hemisphere/src/index.js
@@ -1,18 +1,33 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import HemisphereDisplay from "./hemisphereDisplay";
+//import HemisphereDisplay from "./hemisphereDisplay";
 
-const App = () => {
-    window.navigator.geolocation.getCurrentPosition(
-        (position) => console.log(position),
-        (error) => console.log(error)
-    );
-    return (
-        <div>
-            latute:
-            <HemisphereDisplay />
-        </div>
-    )
+// const App = () => {
+//     window.navigator.geolocation.getCurrentPosition(
+//         (position) => console.log(position),
+//         (error) => console.log(error)
+//     );
+//     return (
+//         <div>
+//             latute:
+//             <HemisphereDisplay />
+//         </div>
+//     )
+// }
+
+class App extends React.Component{
+  
+    render () {
+        window.navigator.geolocation.getCurrentPosition(
+            (position) => console.log(position),
+            (error) => console.log(error)
+        );
+        return(
+            <div>
+                You are in the northen hemisphere
+            </div>
+        )
+    }
 }
 
 ReactDOM.render(
