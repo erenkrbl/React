5c4f03e493052db12fc755ee632d0efce6a5d343
Joseph Abraham <ekurubal24@gmail.com>
2021-07-01 14:58:38 +0300

    SearchInput, App component created

---
+++ image-list/src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import SearchInput from './SearchInput';
+
+
+const App = () => {
+    return (
+        <div>
+            <SearchInput />
+        </div>
+    )
+}
+
+export default App;
---
+++ image-list/src/components/SearchInput.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+class SearchInput extends React.Component {
+    // constructor(props){
+    //     super(props);
+    //     state: this.state
+    // }
+
+    render(){
+        return (
+            <div>
+                <form action="">
+                    <input type="text"/>
+                </form>
+            </div>
+        )
+    }
+}
+
+export default SearchInput;
