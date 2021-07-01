b5defca9ef865689084fecf7954fb1435fabc540
Joseph Abraham <ekurubal24@gmail.com>
2021-07-01 18:34:20 +0300

    Bind added

--- image-list/src/components/SearchInput.js
+++ image-list/src/components/SearchInput.js
@@ -1,21 +1,36 @@
 import React from 'react';
 
 class SearchInput extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { entry: '' };
+
+        this.onFormSubmit = this.onFormSubmit.bind(this);
+    }
 
     // onInputChange = (event) => {
     //     console.log(event.target.value)
     // }
+    
+
+    onFormSubmit(event) {
+        event.preventDefault();
+        console.log(this.state.entry);
+    } 
+    
 
     render(){
         return (
             <div className='ui segment'>
-                <form className='ui form'>
+                <form onSubmit={this.onFormSubmit} className='ui form'>
                     <div className='field'>
                         <div className='ui massive icon input'>
                             <input 
                                 type="text" 
                                 placeholder='search...' 
-                                onChange={(event) => console.log(event.target.value)}/>
+                                onChange={(event) => this.setState({entry: event.target.value})}
+                                value={this.state.entry}
+                            />
                             <i className='search icon'></i>
                         </div>
                     </div>
