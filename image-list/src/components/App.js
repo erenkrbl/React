32d12eeef21f3244e10c727e311fda8cf2d7896f
Joseph Abraham <ekurubal24@gmail.com>
2021-07-02 12:26:55 +0300

    Async await, images added

--- image-list/src/components/App.js
+++ image-list/src/components/App.js
@@ -1,16 +1,22 @@
 import React from 'react';
+import axios from 'axios';
 import SearchInput from './SearchInput';
 
 
 class App extends React.Component  {
-    onSearchSubmit = (entry) => {
-        console.log(entry);
+
+    state = { images: [] }
+
+    onSearchSubmit = async (entry) => {
+        const response = await axios.get(`https://pixabay.com/api/?key=&q=${entry}&image_type=photo`);
+        this.setState({images: response.data.hits })
     }
 
     render() {
         return (
             <div className='ui container' style={{ marginTop: '30px' }}>
                 <SearchInput onSearchSubmit={this.onSearchSubmit} />
+                We have { this.state.images.length } images
             </div>
         )
     }
