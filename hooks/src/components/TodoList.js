3a7cee01507a08ab1dd1c86e2e6fad1ed82ba1a1
Joseph Abraham <ekurubal24@gmail.com>
2021-07-13 14:51:16 +0300

    useState use added

--- hooks/src/components/TodoList.js
+++ hooks/src/components/TodoList.js
@@ -1,13 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const TodoList = () => {
+    const [todos, setTodos] = useState([
+        { text: 'Pay bills', id: 1 },
+        { text: 'Do your homework', id: 2 },
+        { text: 'Feed the dog', id: 3 }
+    ]);
+
+    const addTodo = () => {
+        setTodos([
+            ...todos,
+            { text: 'Learn hooks', id: Math.random()}
+        ]);
+    }
+    
     return (
         <div>
             <ul>
-                <li>Pay bills</li>
-                <li>Do your homework</li>
-                <li>Feed the dog</li>
-            </ul> 
+                { todos.map((todo) =>{
+                    return(<li key={todo.id}>{todo.text}</li>)
+                })}
+            </ul>
+            <button onClick={addTodo}>Add a Todo</button> 
         </div>
     )
 }
