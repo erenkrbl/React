2cc857ab73b134d02cc6d0e9c6c4ef2c6d1570b9
Joseph Abraham <ekurubal24@gmail.com>
2021-07-14 13:24:58 +0300

    class component converted to function component

--- contextapi/src/components/Navbar.js
+++ contextapi/src/components/Navbar.js
@@ -1,40 +1,64 @@
-import React from 'react';
+import React, { useContext} from 'react';
 import { AuthContext } from '../contexts/AuthContext';
 import { ThemeContext } from '../contexts/ThemeContext';
 
-class Navbar extends React.Component {
+// class Navbar extends React.Component {
     
-    render() { 
-        return (
-            <AuthContext.Consumer>{(authContext) => (
-                <ThemeContext.Consumer>{(themeContext)=> {
-                    const { isDarkTheme, darkTheme, lightTheme } = themeContext;
-                    const { isLoggedIn, changeAuthStatus } = authContext
-                    const theme = isDarkTheme ? darkTheme : lightTheme;
-                    return(
-                        <nav style={{ background: theme.background, color: theme.text, height: '120px'  }}>
-                            <h2 style={{ textAlign: 'center'}}>
-                                ClarusWay
-                            </h2>
-                            <p
-                                style={{ textAlign: 'center'}} 
-                                onClick={changeAuthStatus}
-                            >
-                                { isLoggedIn ? 'Logged in' : 'Logged out'}
-                            </p>
-                            <div className='ui three buttons'>
-                                <button className='ui button'>Overview</button>
-                                <button className='ui button'>Contact</button>
-                                <button className='ui button'>Support</button>
-                            </div>
-                        </nav>
-                    )
-                }}
-                </ThemeContext.Consumer>
-            )}
-            </AuthContext.Consumer>
-        )  
-    }
-}
+//     render() { 
+//         return (
+//             <AuthContext.Consumer>{(authContext) => (
+//                 <ThemeContext.Consumer>{(themeContext)=> {
+//                     const { isDarkTheme, darkTheme, lightTheme } = themeContext;
+//                     const { isLoggedIn, changeAuthStatus } = authContext
+//                     const theme = isDarkTheme ? darkTheme : lightTheme;
+//                     return(
+//                         <nav style={{ background: theme.background, color: theme.text, height: '120px'  }}>
+//                             <h2 style={{ textAlign: 'center'}}>
+//                                 ClarusWay
+//                             </h2>
+//                             <p
+//                                 style={{ textAlign: 'center'}} 
+//                                 onClick={changeAuthStatus}
+//                             >
+//                                 { isLoggedIn ? 'Logged in' : 'Logged out'}
+//                             </p>
+//                             <div className='ui three buttons'>
+//                                 <button className='ui button'>Overview</button>
+//                                 <button className='ui button'>Contact</button>
+//                                 <button className='ui button'>Support</button>
+//                             </div>
+//                         </nav>
+//                     )
+//                 }}
+//                 </ThemeContext.Consumer>
+//             )}
+//             </AuthContext.Consumer>
+//         );  
+//     };
+// };
+
+const Navbar = () => {
+    const { isDarkTheme, darkTheme, lightTheme } = useContext(ThemeContext);
+    const { isLoggedIn, changeAuthStatus } = useContext(AuthContext);
+    const theme = isDarkTheme ? darkTheme : lightTheme;
+    return(
+        <nav style={{ background: theme.background, color: theme.text, height: '120px'  }}>
+            <h2 style={{ textAlign: 'center'}}>
+                ClarusWay
+            </h2>
+            <p
+                style={{ textAlign: 'center'}} 
+                onClick={changeAuthStatus}
+            >
+                { isLoggedIn ? 'Logged in' : 'Logged out'}
+            </p>
+            <div className='ui three buttons'>
+                <button className='ui button'>Overview</button>
+                <button className='ui button'>Contact</button>
+                <button className='ui button'>Support</button>
+            </div>
+        </nav>
+    );
+};
 
 export { Navbar };
