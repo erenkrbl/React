49a7b6e915d95be22d575c56ed432b1da7824ebb
Joseph Abraham <ekurubal24@gmail.com>
2021-07-29 16:32:34 +0300

    Created List and App component

--- birthday-reminder/src/App.js
+++ birthday-reminder/src/App.js
@@ -2,7 +2,14 @@ import React, { useState } from 'react';
 import data from './data';
 import List from './List';
 function App() {
-  return <h2>reminder project </h2>;
+  const [people, setPeople] = useState(data);
+  return <main>
+    <section className="container">
+      <h3>{people.length} birtdays today</h3>
+      <List people={people}/>
+      <button onCli={() => console.log('you click me')}>Clear All</button>
+    </section>
+  </main>;
 }
 
 export default App;
