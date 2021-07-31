7d9a4f06c44df9fa5bde301ff5446ceb1c8a3d6c
Joseph Abraham <ekurubal24@gmail.com>
2021-07-31 14:01:14 +0300

    Project start

---
+++ menu/src/App.js
@@ -0,0 +1,15 @@
+import React, { useState } from 'react';
+import Menu from './Menu';
+import Categories from './Categories';
+import items from './data';
+
+function App() {
+  const [menuItems, setMenuItems] = useState(items);
+  const [categories, setCategories] = useState([]);
+  return (
+  <main>
+    Our Menu
+  </main>);
+}
+
+export default App;
