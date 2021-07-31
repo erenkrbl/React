454dda3a3ef7c55e235bf5a4e0aec4ae554e20af
Joseph Abraham <ekurubal24@gmail.com>
2021-07-31 15:03:32 +0300

    All button added (dynamic approach)

--- menu/src/App.js
+++ menu/src/App.js
@@ -3,11 +3,20 @@ import Menu from './Menu';
 import Categories from './Categories';
 import items from './data';
 
+
+const allCategories = ['all', ...new Set(items.map((item) => item.category))];
+
 function App() {
   const [menuItems, setMenuItems] = useState(items);
-  const [categories, setCategories] = useState([]);
+  const [categories, setCategories] = useState(allCategories);
+
+  
 
   const filterItems = (category) => {
+    if(category ==='all') {
+      setMenuItems(items);
+      return;
+    }
     const newItems = items.filter((item) => item.category === category);
     setMenuItems(newItems);
   }
@@ -18,7 +27,7 @@ function App() {
         <h2>Our Menu</h2>
         <div className='underline'></div>
       </div>
-    <Categories filterItems={filterItems} />
+    <Categories filterItems={filterItems} categories={categories} />
     <Menu items={menuItems} /> 
     </section>
   </main>);
