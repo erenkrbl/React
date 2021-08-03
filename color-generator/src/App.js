4654244fb0958fd02dbf09e14d688b874966bbfe
Joseph Abraham <ekurubal24@gmail.com>
2021-08-03 16:01:22 +0300

    Single color added

--- color-generator/src/App.js
+++ color-generator/src/App.js
@@ -12,8 +12,8 @@ function App() {
     e.preventDefault();
     try {
       let colors = new Values(color).all(10)
-      console.log(colors)
-      
+      setList(colors)
+      //console.log(colors)  
     } catch (error) {
       setError(true)
       console.log(error)
@@ -37,7 +37,18 @@ function App() {
         </form> 
       </section>
       <section className='colors'>
-        <h4>list goes here</h4>
+        
+        {list.map((color, index) => {
+          //console.log(color)
+          return (
+            <SingleColor 
+              key={index} 
+              {...color} 
+              index={index} 
+              hexColor={color.hex} 
+            />
+          )
+        })}
       </section>
     </>
     )
