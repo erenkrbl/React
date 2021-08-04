2f5e40aec7f581a26064c71c841e06e21fac8b30
Joseph Abraham <ekurubal24@gmail.com>
2021-08-04 23:04:40 +0300

    Edit item added

--- Grocery-Bud/src/Alert.js
+++ Grocery-Bud/src/Alert.js
@@ -1,12 +1,12 @@
 import React, { useEffect } from 'react'
 
-const Alert = ({type, msg, removeAlert}) => {
-  useEffect(() =>{
+const Alert = ({type, msg, removeAlert, list}) => {
+  useEffect(() => {
     const timeout = setTimeout(() => {
       removeAlert()  
     }, 3000);
     return () => clearTimeout(timeout)
-  }, [])
+  }, [list])
   return (
     <p className={`alert alert-${type}`}>
       {msg}
--- Grocery-Bud/src/App.js
+++ Grocery-Bud/src/App.js
@@ -21,11 +21,23 @@ function App() {
     } 
     else if (name && isEditing) {
       //deal with edit
+      setList(
+        list.map((item) => {
+          if(item.id === editID) {
+            return {...item, title:name}
+          }
+          return item
+        })
+      )
+      setName('');
+      setEditID(null);
+      setIsEditing(false);
+      showAlert(true, 'success', 'value changed')
       
     }
     else {
       // show alert
-      showAlert(false, 'success', 'items added to list')
+      showAlert(true, 'success', 'items added to list')
       const newItem = {id: new Date().getTime().toString(), title:name};
       setList([...list, newItem]);
       setName('')
@@ -47,10 +59,18 @@ function App() {
     showAlert(true, 'danger', 'item removed')
     setList(list.filter((item) => item.id !== id))
   }
+
+  const editItem = (id) => {
+    const specificItem = list.find((item) => item.id === id);
+    setIsEditing(true);
+    setEditID(id);
+    setName(specificItem.title); 
+
+  }
   return (
     <section className='section-center'>
       <form className='grocery-form' onSubmit={handleSubmit}>
-        {alert.show && <Alert {...alert} removeAlert={showAlert} />}
+        {alert.show && <Alert {...alert} removeAlert={showAlert} list={list}/>}
         <h3>grocery bud</h3>
         <div className='form-control'>
           <input 
@@ -68,7 +88,7 @@ function App() {
       </form>
       {list.length > 0 && (
         <div className='grocery-container'>
-          <List items={list} removeItem={removeItem} />
+          <List items={list} removeItem={removeItem} editItem={editItem} />
           <button className='clear-btn' onClick={clearList}>clear items</button>
         </div>
       )}
