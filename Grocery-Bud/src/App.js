326d74c8d2077ca50f05fdd5e1f31ae37d9dce88
Joseph Abraham <ekurubal24@gmail.com>
2021-08-04 22:30:10 +0300

    Remove item added

--- Grocery-Bud/src/App.js
+++ Grocery-Bud/src/App.js
@@ -42,6 +42,11 @@ function App() {
     showAlert(true, 'danger', 'empty list');
     setList([]);
   }
+
+  const removeItem = (id) => {
+    showAlert(true, 'danger', 'item removed')
+    setList(list.filter((item) => item.id !== id))
+  }
   return (
     <section className='section-center'>
       <form className='grocery-form' onSubmit={handleSubmit}>
@@ -63,7 +68,7 @@ function App() {
       </form>
       {list.length > 0 && (
         <div className='grocery-container'>
-          <List items={list} />
+          <List items={list} removeItem={removeItem} />
           <button className='clear-btn' onClick={clearList}>clear items</button>
         </div>
       )}
