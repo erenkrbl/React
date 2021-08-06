46d46b521a8c6c804327078d0770e63ddf1f4b2a
Joseph Abraham <ekurubal24@gmail.com>
2021-08-06 23:18:44 +0300

    UseEffect, usRef added

--- navbar/src/App.js
+++ navbar/src/App.js
@@ -1,11 +1,10 @@
 import React from 'react'
-import Navbar from './Navbar'
-import Sidebar from './Sidebar'
+import Navbar from './Navbar';
+
 function App() {
   return (
     <> 
-      <Navbar />
-      <Sidebar />  
+      <Navbar /> 
     </>
   )
 }
