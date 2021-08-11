db8a849bb8b431818e15c8d32aba0d664dc6e35f
Joseph Abraham <ekurubal24@gmail.com>
2021-08-11 14:38:03 +0300

    Context Boilerplate created

--- stripe-menu/src/context.js
+++ stripe-menu/src/context.js
@@ -1,2 +1,41 @@
 import React, { useState, useContext } from 'react'
-import sublinks from './data'
+import sublinks from './data';
+
+const AppContext =React.createContext();
+
+
+
+export const AppProvider = ({ children }) => {
+    const [isSideBarOpen, setSideBarOpen] = useState(true);
+    const [isSubMenuOpen, setSubMenuOpen] = useState(true);
+
+    const openSideBar = () => {
+        setSideBarOpen(true);
+    }
+    const closeSideBar = () => {
+        setSideBarOpen(false);
+    }
+    const openSubMenu = () => {
+        setSubMenuOpen(true);
+    }
+    const closeSubMenu = () => {
+        setSubMenuOpen(false);
+    }
+    return (
+        <AppContext.Provider  value={{
+            isSideBarOpen, 
+            isSubMenuOpen, 
+            openSideBar, 
+            closeSideBar, 
+            openSubMenu,
+            closeSubMenu, 
+        }}
+        >
+            {children}
+        </AppContext.Provider>
+    )
+}
+
+export const useGlobalContext = () => {
+    return useContext(AppContext)
+}
