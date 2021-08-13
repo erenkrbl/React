8a1438c340ca11e15c4d0a1e4847b56664cb54a2
Joseph Abraham <ekurubal24@gmail.com>
2021-08-13 18:35:01 +0300

    Sublinks added

--- stripe-menu/src/Submenu.js
+++ stripe-menu/src/Submenu.js
@@ -2,8 +2,13 @@ import React, { useState, useRef, useEffect } from 'react'
 import { useGlobalContext } from './context'
 
 const Submenu = () => {
-  const { isSubMenuOpen, location } = useGlobalContext();
+  const { 
+    isSubMenuOpen, 
+    page : { page, links },
+    location, 
+  } = useGlobalContext();
   const container = useRef(null);
+  const [colums, setColums] = useState('col-2')
 
   useEffect(() => {
     const subMenu = container.current;
@@ -14,8 +19,21 @@ const Submenu = () => {
   }, [location])
 
   return (
-    <aside className={`${isSubMenuOpen ? 'submenu show' : 'submenu'}`} ref={container}>
-      submenu component
+    <aside 
+      className={`${isSubMenuOpen ? 'submenu show' : 'submenu'}`} 
+      ref={container}>
+      <h4>{page}</h4>
+      <div className={`submenu-center ${colums}`}>
+        {links.map((link, index) => {
+          const { icon, label, url} = link;
+          return (
+            <a key={index} href={url}>
+              {icon}
+              {label}
+            </a>
+          )
+        })}
+      </div>
     </aside>
   )
 }
