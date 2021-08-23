9dc944dba8784f9ecedc1dc440cb83cdcfc3a8db
Joseph Abraham <ekurubal24@gmail.com>
2021-08-23 17:40:20 +0300

    Added Marker, Popup

--- map-app/src/App.js
+++ map-app/src/App.js
@@ -1,8 +1,12 @@
 import React, { useState, useRef } from "react";
-import { MapContainer, TileLayer } from "react-leaflet";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import cities from './cities.json';
 import "./styles.css";
 
+
+
 function App() {
+
   const [center, setCenter] = useState({ lat: 51.505, lng: -0.09 });
   const ZOOM_LEVEL = 9;
   const mapRef = useRef();
@@ -18,7 +22,20 @@ function App() {
         attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-    </MapContainer>
+      {cities.map((city, index) =>
+        <Marker
+          key={index}
+          position={[city.lat, city.lng]}
+        >
+          <Popup>
+            <b>{city.city}<br/>{city.country}</b>
+          </Popup>
+
+        </Marker>
+      )}
+
+
+    </MapContainer >
   );
 }
 
