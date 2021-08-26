08ddce206ad1da4562cfc751e9e8612df168f2fa
Joseph Abraham <ekurubal24@gmail.com>
2021-08-26 15:05:43 +0300

    Created map components

---
+++ map/src/components/Map.js
@@ -0,0 +1,38 @@
+import React from "react";
+import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
+import "./styles.css";
+
+function Map() {
+  const menti = [1.35735, 103.7];
+  const locations = [
+    { name: "menti", position: [1.35735, 103.7], size: 40 },
+    { name: "mentor", position: [1.35735, 103.94], size: 30 },
+    { name: "mentor", position: [1.35735, 103.8], size: 30 }
+  ];
+  return (
+    <MapContainer
+      className="leaflet-container"
+      center={menti}
+      zoom={13}
+      scrollWheelZoom={false}
+    >
+      <TileLayer
+        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      {locations.map((location, index) => {
+        return (
+          <Marker position={location.position} key={index}>
+            <Popup>
+              Name: {location.name} <br />
+              latitude: {location.position[0]}
+              <br />
+              longtitude: {location.position[1]}
+            </Popup>
+          </Marker>
+        );
+      })}
+    </MapContainer>
+  );
+}
+export default Map;
